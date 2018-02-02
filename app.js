1c4a3371adef384fdda06fb18736ea4bd3d1226c
eak2 <eakkachai@mycostech.com>
2018-02-02 17:31:36 +0700

    build HDB Studio with JadeJS

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var express = require('express')
-var app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+var express = require('express')
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'resources')));
+
+app.set('views', path.join(__dirname + '/pages'));
+app.set('view engine', 'jade')
+
+app.get('/main', function (req,res) {
+    res.render('main', {youAreUsingJade:true});
+});
+
+app.get('/explore', function (req,res) {
+    res.render('explore');
+});
+
+app.get('/explorefilter', function (req,res) {
+    res.render('explorefilter');
+});
+
+app.get('/', function (req,res) {
+    res.render('index');
+});
 
 app.listen(61183, function () {
     console.log('Example app listening on port 61183!')
 })
+
