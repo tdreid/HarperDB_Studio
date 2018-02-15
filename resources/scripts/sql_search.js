7eed6b8369296829dbeff8c5dccc7ccf2ba7db2e
nuenook <nook.ittipon@gmail.com>
2018-02-15 16:46:32 +0700

    set simple autocomplete

--- resources/scripts/sql_search.js
+++ resources/scripts/sql_search.js
@@ -1,46 +1,81 @@
 // #exampleTextarea
 $(document).ready(function () {
-var Textarea = Textcomplete.editors.Textarea;
-var textareaElement = document.getElementById('exampleTextarea')
-var editor = new Textarea(textareaElement);
+    var Textarea = Textcomplete.editors.Textarea;
+    var textareaElement = document.getElementById('exampleTextarea')
+    var editor = new Textarea(textareaElement);
 
-var textcomplete = new Textcomplete(editor, {
-    dropdown: {
-      maxCount: Infinity
+    var textcomplete = new Textcomplete(editor, {
+        dropdown: {
+            maxCount: Infinity
+        }
+    });
+    var curDotName = null;
+
+    checkIsGetChild = (text) => {
+        var match;
+        match = text.match(/\b(\w+\.\w*)$/g);
+        if (match == null)
+            return true;
+        else {
+            var dot = match[0].lastIndexOf(".");
+            curDotName = match[0].substring(0, dot);
+            return false;
+        }
+
     }
-  })
 
-  textcomplete.register([
-    { // tech companies
-        wordsBegin: ['SELECT', 'INSERT', 'UPDATE', 'FROM', 'WHERE'],
-        // match: /.\b(\w{1,})$/,
-        match: /\b(\w{1,})$/,
-        search: function (term, callback) {
-            callback($.map(this.wordsBegin, function (word) {            
-                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-            }));
+    arrayOf = {
+        dog: [
+            'breed',
+            'name',
+            'age'
+        ],
+        dev: [
+            'id',
+            'gender',
+            'foo'
+        ]
+    }
+    textcomplete.register([{
+            wordsBegin: ['SELECT', 'INSERT', 'UPDATE', 'FROM', 'WHERE', 'INTO', 'SET', 'dog'],
+            match: /\b(\w+)$/,
+            search: function (term, callback) {
+                callback($.map(this.wordsBegin, function (word) {
+                    return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
+                }));
+            },
+            index: 1,
+            replace: function (word) {
+                curDotName = word;
+                return word;
+            },
+            context: (text) => {
+                return checkIsGetChild(text);
+            }
         },
-        index: 1,
-        replace: function (word) {
-            return word;
-        }
-    },
+        {
+            wordsBeginssss: arrayOf,
+            match: /\b(\.\w*)$/,
+            search: function (term, callback) {
+                var dot = term.indexOf('.');
+                var fontTerm = term.substring(0, dot - 1);
+                var useWordArray = this.wordsBeginssss[curDotName];
+                callback($.map(useWordArray, function (word) {
 
-    { // tech companies
-        wordsBegin: ['1111111111', '222', '3', '4', '5'],
-        // match: /.\b(\w{1,})$/,
-        match: /\b(\w{5,})$/,
-        search: function (term, callback) {
-            callback($.map(this.wordsBegin, function (word) {            
-                return word.toLowerCase().indexOf(term.toLowerCase()) === 0 ? word : null;
-            }));
+                    var subTerm = term.substring(dot + 1, term.length + 1);
+                    if (subTerm.length > 0)
+                        return word.toLowerCase().indexOf(subTerm.toLowerCase()) === 0 ? word : null;
+                    else return word;
+
+                }));
+            },
+            index: 1,
+            replace: function (word, term) {
+
+                return '.' + word;
+            }
         },
-        index: 1,
-        replace: function (word) {
-            return word;
-        }
-    },
 
-]);
+    ]);
 
 });
