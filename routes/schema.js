eac880cd70d91e64585f001d2bf1c2d2e5ac5397
unknown <nook.ittipon@gmail.com>
2018-02-16 09:32:29 +0700

    add a schema_name page

--- routes/schema.js
+++ routes/schema.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res) {
     res.render('schema');
 });
 
+router.get('/:schemaName', function(req, res) {
+    console.log(req.params);
+    res.render('schema_name', {schemaName: req.params.schemaName});
+});
+
 module.exports = router;
