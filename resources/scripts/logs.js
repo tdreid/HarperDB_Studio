05015b74642c397006de2fd76ee7bb4a8821694b
unknown <nook.ittipon@gmail.com>
2018-03-21 17:45:17 +0700

    search logs

--- resources/scripts/logs.js
+++ resources/scripts/logs.js
@@ -1,19 +1,16 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    var logTable = $('#logsTable').DataTable({
-        "dom": "<'row'<'col-md-12't><'col-md-4'<'pull-left'l>><'col-md-8 right-pagging'p>>",
-        "lengthMenu": [ [10, 50,100, -1], [10, 50, 100,  "All"] ],
+    var sTable = $('#logsTable').DataTable({
+        "dom": "<'col-md-12't><'col-md-4'<'pull-left'l>><'col-md-8 right-pagging'p>",
+        "lengthMenu": [
+            [10, 50, 100, -1],
+            [10, 50, 100, "All"]
+        ],
         "iDisplayLength": 10,
     });
 
     $('#searchlogs').keyup(function () {        
-        logTable.search($(this).val()).draw();
+        sTable.search($(this).val()).draw();
     });
 
 });
-
-
-
-
-
-
